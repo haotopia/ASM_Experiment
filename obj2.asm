78f66367734b1b3ab9a8f0784ed34c4c657bb3a5
haotopia <33610422+haotopia@users.noreply.github.com>
2018-11-17 10:33:18 +0800

    Update obj2.asm
    BUG修复

--- obj2.asm
+++ obj2.asm
@@ -1,17 +1,22 @@
 DATAS SEGMENT
-    ;此处输入数据段代码  
+    ;此处输入数据段代码
+
+    ;提示字段 
     stringkey db 'Enter keyword:$'
     stringsen db 'Enter Sentence:$'
     stringloc db 'Match at localtion:$'
     stringnom db 'No match.$'
     stringout db 'H of the sentence.$'
+    ;删除关键字
+    stringnew db 'New sentence.$'
     
-    inputkey label byte
+    ;输入字段
+    inputkey label byte     ;关键词
     max1 db 10
     act1 db ?
     keyword db 10 dup(?)
     
-    inputsen label byte
+    inputsen label byte     ;句子
     max2 db 50
     act2 db ?
     sentence db 50 dup(?)
@@ -93,86 +98,93 @@ START:
     mov si,0
     
 compare:
-	mov ah,[bx+di]
-	cmp ah,keyword[si]
-	jne next
-	
-	;没遇到一个相等的字符cx-1
-	inc di
-	inc si
-	dec cx
-	
+    mov ah,[bx+di]
+    cmp ah,keyword[si]
+    jne next
+    
+    ;没遇到一个相等的字符cx-1
+    inc di
+    inc si
+    dec cx
+
+    cmp cx,0
+    je oute
+    jmp compare
+    
 next:
-	;bx+1清空si,di继续关键词比较
-	inc bx
-	dec al
-	cmp al,0
-	je ende
-	
-	mov di,0
-	mov si,0
-	pop cx
-	push cx
-	jmp compare
-	
+    ;bx+1清空si,di继续关键词比较
+    inc bx
+    dec al
+    cmp al,0
+    je ende
+    
+    mov di,0
+    mov si,0
+    pop cx
+    push cx
+    jmp compare
+    
 oute:
-	sub bx,offset sentence
-	inc bx
-	
-	push dx
-	mov dl,0dh
-	mov ah,02h
-	int 21h
-	pop dx
-	
-	lea dx,stringloc
-	mov ah,09h
-	int 21h
-	
-	;转子程序获取关键位置，H
-	call btoh
-	
-	lea dx,stringout
-	mov ah,09
-	int 21h
-	jmp start
-	
-	;B->H
+    sub bx,offset sentence
+    inc bx
+    
+    push dx
+    mov dl,0dh
+    mov ah,02h
+    int 21h
+    mov dl,0ah
+    mov ah,02h
+    int 21h
+    pop dx
+    
+    lea dx,stringloc
+    mov ah,09h
+    int 21h
+    
+    ;转子程序获取关键位置，H
+    call btoh
+    
+    lea dx,stringout
+    mov ah,09
+    int 21h
+    jmp start
+    
+    ;B->H
 btoh PROC NEAR
-	mov ch,4
+    mov ch,4
 rotate:
-	mov cl,4
-	rol bx,cl
-	mov al,bl
-	and al,0fh
-	add al,30h
-	cmp al,3ah
-	jl print
-	add al,7h
+    mov cl,4
+    rol bx,cl
+    mov al,bl
+    and al,0fh
+    add al,30h
+    cmp al,3ah
+    jl print
+    add al,7h
 print:
-	mov dl,al
-	mov ah,02h
-	int 21h
-	dec ch
-	jnz rotate
-	ret
+    mov dl,al
+    mov ah,02h
+    int 21h
+    dec ch
+    jnz rotate
+    ret
 btoh endp
 
 ende:
-	push dx
-	mov dl,0dh
-	mov ah,02h
-	int 21h
-	mov dl,0ah
-	mov ah,02h
-	int 21h
-	pop dx
-	
-	lea dx,stringnom
-	mov ah,09
-	int 21h
-	jmp start
-	
+    push dx
+    mov dl,0dh
+    mov ah,02h
+    int 21h
+    mov dl,0ah
+    mov ah,02h
+    int 21h
+    pop dx
+    
+    lea dx,stringnom
+    mov ah,09
+    int 21h
+    jmp start
+    
     MOV AH,4CH
     INT 21H
 CODES ENDS
